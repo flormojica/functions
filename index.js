185895c6e5db38f8366c92ced5462200f4d93081
flormojica <flormojica00@gmail.com>
2018-06-29 13:41:50 -0400

    adding

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 function add (a,b){
-return (a+b);
+let result= 0;
+for(let i = 0; i < b; i++){
+    result = a+b;
 }
-function multiply (a,b){
-var count = 0;
-    for(i=0; i<a; i++){
-count = add(count,b);
+return result;
 }
-return (count);
+const answer = add(2,4)
+document.getElementById('Addition').innerHTML = "your total is: "+answer;
+
+function multiply (a, b){
+        let result2 = 0;
+    for (let i = 0; i < b; i++){
+         add(a, result2);
+         result2= result2 + a
+    }
+  return result2;
 }
+const numbers = multiply(6,8);
+document.getElementById('Multiplication').innerHTML = "your total is: "+numbers;
+
+function power(x, n){
+let total = x;
+for (let i = 1; i < n; i++){
+    total = total * x;
+}
+return total;  
+}
+const powered = power(2, 8);
+document.getElementById('Power').innerHTML = 'Your Total is: '+powered;
